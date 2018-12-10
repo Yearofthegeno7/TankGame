94c3730685a5c048d6de5f8275843a695605e211
kobayashi14 <kobayashi1254@students.d211.org>
2018-12-10 14:22:46 -0600

    Added three methods.
    add_projectile(mass, radius, x-coordinate, y-coordinate, id number)
    
    add_target(mass, radius, x-coordinate, y-coordinate, id number)
    
    add_target_part(mass, radius, x-coordinate, y-coordinate, id number)

--- Main.py
+++ Main.py
@@ -12,36 +12,89 @@ import time
 space = pymunk.Space()
 space.gravity = (0, 0)
 
-body = pymunk.Body(1, 1666, pymunk.Body.STATIC)
-body.position = 0, 0
-poly = pymunk.Poly.create_box(body, size=(800, 20))
-space.add(body, poly)
-mass = 1
-radius = 30
-angle = 45
+# body = pymunk.Body(1, 1666, pymunk.Body.STATIC)
+# body.position = 0, 0
+# poly = pymunk.Poly.create_box(body, size=(800, 20))
+# space.add(body, poly)
+# mass = 1
+# radius = 30
+# angle = 45
 
-circle_moment = pymunk.moment_for_circle(mass, 0, radius)
-circle_body = pymunk.Body(mass, circle_moment)
-circle_body.position = 50, 50
-circle_shape = pymunk.Circle(circle_body, radius)
-circle_shape.elasticity = 0.8
-circle_shape.friction = 1.0
-space.add(circle_body, circle_shape)
+# ground beneath the ball. Static object that ball cannot phase through
+segment_shape = pymunk.Segment(space.static_body, (0, 0), (800, 0), 2)
+segment_shape.body.position = 10, 10
+segment_shape.elasticity = 0.98
+segment_shape.friction = 1.0
+segment_shape.id = -1
+space.add(segment_shape)
+
+angle = 45
+targets = []
+projectiles = []
+target_parts = []
+
+
+# creates the projectile that the tank launches
+def add_projectile(x, y):
+    mass = 1
+    radius = 15
+    moment = pymunk.moment_for_circle(mass, 0, radius)
+    body = pymunk.Body(mass, moment)
+    body.position = 50, 50
+    shape = pymunk.Circle(body, radius)
+    shape.elasticity = 0.8
+    shape.friction = 1.0
+    shape.id = 10
+    space.add(body, shape)
+    projectiles.append(shape)
+
+
+# creates the targets
+def add_target(mass, radius, x, y, i):
+    moment = pymunk.moment_for_circle(mass, 0, radius)
+    body = pymunk.Body(mass, moment, pymunk.Body.KINEMATIC)
+    body.position = x, y
+    shape = pymunk.Circle(body, radius)
+    shape.elasticity = 0.8
+    shape.friction = 1.0
+    shape.id = i
+    space.add(body, shape)
+    targets.append(shape)
+
+
+# creates the parts of a target (used when the target is shot and breaks)
+def add_target_part(mass, radius, x, y, i):
+    moment = pymunk.moment_for_circle(mass, 1667, radius)
+    body = pymunk.Body(mass, moment)
+    body.position = x, y
+    shape = pymunk.Circle(body, radius)
+    shape.elasticity = 0.8
+    shape.friction = 1.0
+    shape.id = i
+    space.add(body, shape)
+    target_parts.append(shape)
+
+
+# circle_moment = pymunk.moment_for_circle(mass, 0, radius)
+# circle_body = pymunk.Body(mass, circle_moment)
+# circle_body.position = 50, 50
+# circle_shape = pymunk.Circle(circle_body, radius)
+# circle_shape.elasticity = 0.8
+# circle_shape.friction = 1.0
+# space.add(circle_body, circle_shape)
 
 def main():
     rot = 0
-    movex=0
-    movey=0
+    movex = 0
+    movey = 0
 
     rot_speed = 2
     where = 118, 70
     where2x = 100
-    where2y=90
+    where2y = 90
     pygame.init()
     clock = pygame.time.Clock()
     clock.tick(5)
-
-
 
     # create new surface with white BG
     surf = pygame.Surface((5, 50))
@@ -55,13 +108,13 @@ def main():
     # create shapes so you can tell rotation is happenning
     bigger = pygame.Rect(0, 0, 5, 50)
     smaller = pygame.Rect(0, 0, 40, 40)
-    #draw those two shapes to that surface
+    # draw those two shapes to that surface
     pygame.draw.rect(surf, (100, 0, 0), bigger)
     pygame.draw.rect(surf2, (0, 100, 0), smaller)
 
     screen = pygame.display.set_mode((400, 400))
     blittedRect = screen.blit(surf, where)
-    blittedRect2 = screen.blit(surf2, (where2x,where2y))
+    blittedRect2 = screen.blit(surf2, (where2x, where2y))
 
     pygame.font.init()
     myfont = pygame.font.SysFont('Comic Sans MS', 20)
@@ -69,9 +122,7 @@ def main():
     global angle
     draw_options = pymunk.pygame_util.DrawOptions(screen)
 
-
     while True:
-
 
         pygame.time.delay((100))
         for event in pygame.event.get():
@@ -88,11 +139,12 @@ def main():
             if rot >= -45:
                 rot = (rot - rot_speed)
         if keys[pygame.K_d]:
-            where2x+=5
+            where2x += 5
             blittedRect.centerx += 5
         if keys[pygame.K_SPACE]:
             space.gravity = (0, -980)
-            circle_body.apply_impulse_at_world_point((100 * math.cos(radians(angle)), 100 * math.sin(radians(angle))), (50, 50))
+            projectiles[0].body.apply_impulse_at_world_point((100 * math.cos(radians(angle)), 100 * math.sin(radians(angle))),
+                                                     (50, 50))
         if keys[pygame.K_q] and angle < 90:
             angle = angle + 1
         if keys[pygame.K_e] and angle > 0:
@@ -101,20 +153,19 @@ def main():
         space.step(1 / 50.0)
         screen.fill((0, 0, 0))
         space.debug_draw(draw_options)
-        #pygame.draw.line(screen, (255, 255, 255), (0, 0), (400, 400))
+        # pygame.draw.line(screen, (255, 255, 255), (0, 0), (400, 400))
         # text = 'Angle: ' + str(angle)
         textsurface = myfont.render('Angle: ' + str(angle), False, (255, 255, 255))
         screen.blit(textsurface, (10, 10))
         textsurface = myfont.render('Force: 100', False, (255, 255, 255))
         screen.blit(textsurface, (10, 40))
         pygame.display.flip()
-        #clock.tick(40)
-        print(circle_body.position.y)
-
+        # clock.tick(40)
+        # print(circle_body.position.y)
 
         #    blittedRect.
-        #screen = pygame.display.set_mode((400, 400))
-       # screen.fill((0, 0, 0))
+        # screen = pygame.display.set_mode((400, 400))
+        # screen.fill((0, 0, 0))
 
         ##ROTATED
         # get center of surf for later
@@ -128,8 +179,9 @@ def main():
         rotRect.center = oldCenter
         # draw rotatedSurf with the corrected rect so it gets put in the proper spot
         blittedRect = screen.blit(rotatedSurf, rotRect)
-        blittedRect2 = screen.blit(surf2, (where2x,where2y))
+        blittedRect2 = screen.blit(surf2, (where2x, where2y))
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
